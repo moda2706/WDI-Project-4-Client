bc3c3647e2b547ef643f998e379083b6e2ecead0
Sophie2525 <blu.heart87@gmail.com>
2017-06-08 00:10:34 +0100

    more views added

--- src/js/main.controller.js
+++ src/js/main.controller.js
@@ -3,20 +3,38 @@ angular
 .controller('MainCtrl', MainCtrl);
 
 
+// MainCtrl.$inject = ['$rootScope', 'CurrentUserService', '$state'];
+// function MainCtrl($rootScope, CurrentUserService, $state) {
+//   const vm = this;
+//   CurrentUserService.getUser();
+//   $rootScope.$on('LoggedIn', () => {
+//     vm.user = CurrentUserService.currentUser;
+//     vm.loggedIn = true;
+//   });
+//   vm.logout = () => {
+//     vm.loggedIn = false;
+//     CurrentUserService.removeUser();
+//   };
+//   $rootScope.$on('LoggedOut', () => {
+//     vm.user = null;
+//     $state.go('home');
+//   });
+// }
+
 MainCtrl.$inject = ['$rootScope', 'CurrentUserService', '$state'];
 function MainCtrl($rootScope, CurrentUserService, $state) {
   const vm = this;
-  CurrentUserService.getUser();
-  $rootScope.$on('LoggedIn', () => {
-    vm.user = CurrentUserService.currentUser;
-    vm.loggedIn = true;
+
+  $rootScope.$on('loggedIn', () => {
+    vm.user = CurrentUserService.getUser();
   });
+
+  $rootScope.$on('loggedOut', () => {
+    vm.user = null;
+    $state.go('login');
+  });
+
   vm.logout = () => {
-    vm.loggedIn = false;
     CurrentUserService.removeUser();
   };
-  $rootScope.$on('LoggedOut', () => {
-    vm.user = null;
-    $state.go('home');
-  });
 }
